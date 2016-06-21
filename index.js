d94c400a176ecc81f7509a31f647ddebccb8b5ad
Adrenaline3000 <vtmoorek1@gmail.com>
2016-06-21 16:12:51 +0000

    add time server return time code

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
-var port = process.env.PORT||5000;
+var port = process.env.PORT || 5000;
 
-app.get("/", function (request, response) {
-    response.send("Time start");
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.get("/", function(request, response) {
+    var dateObj = {"unix": null, "natural": null};
+    response.send(dateObj);
 });
 
-app.listen(port, function () {
+app.get("/:DATE", function(request, response) {
+    var date = request.params.DATE;
+    var dateObj = {"unix": null, "natural": null};
+
+    if (date) {
+        var dObj = new Date(date);
+        
+        if (Number.parseInt(date))
+        {
+            dObj = new Date(+date);
+        }
+        
+        if (dObj.getTime())
+        {
+            var timeFull = dObj.getTime();
+            var dateStr = monthNames[dObj.getMonth()] + " " + dObj.getDate() 
+            + ", " + dObj.getFullYear();
+            dateObj = {"unix": timeFull, "natural": dateStr};
+        }
+        
+    }
+    
+    response.send(dateObj);
+
+});
+
+app.listen(port, function() {
     console.log("App listening on port " + port);
 });
-
-
